b1aaab2cfcb7bf61c9f6f52806604004fc4af1d7
huythoi123 <92706432+huythoi123@users.noreply.github.com>
2022-11-05 14:54:28 +0700

    Add files via upload

---
+++ check.js
@@ -0,0 +1,40 @@
+function handleLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    users.forEach((user) => {
+      if (user.username == username && user.password == password) {
+        alert("dang nhap thanh cong");
+        switch (user.chucvu) {
+            case "giangvien":
+                window.location.href = "./giangvien.html"
+                break;
+        
+            case "giangvien":
+                window.location.href = "./giangvien.html"
+                break;
+            
+            case "giangvien":
+                window.location.href = "./giangvien.html"
+                break;    
+        }
+      }
+    });
+  }
+
+  var users = [
+    {
+        "chucvu": "giangvien",
+        "username": "lhhuy1",
+        "password": "123"
+    },
+    {
+        "chucvu": "giaovu",
+        "username": "lhhuy2",
+        "password": "123"
+    },
+    {
+        "chucvu":"sinhvien",
+        "username":"lhhuy3",
+        "password":"123"
+    }
+   ]
